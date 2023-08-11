9153b9fa8c250c1db4a6b1144a682667b26aeeaa
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-08-11 17:05:42 +0300

    UPDATE

--- projects/ConvertStringToCamelCase/src/main.cpp
+++ projects/ConvertStringToCamelCase/src/main.cpp
@@ -1,12 +1,62 @@
 #include "base.h"
-#include "sockets.h"
 
-#define DEFAULT_PORT "27015"
-
-int __cdecl main(void)
+bool contains(const vector<char>& items, char item)
 {
-    WSADATA wsaData;
-    auto result = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (result != 0) return 1;
+    for (auto const& i : items)
+        if (i == item) return true;
+    return false;
+}
+string concat(const vector<string>& words)
+{
+    auto result = string("");
+    for (auto const& word : words)
+        result.append(word);
+    return result;
+}
+vector<string> split_2(const string& text, const vector<char>& chars)
+{
+    auto textLenght = text.length();
+    auto words = vector<string>();
+    if (textLenght == 0) return words;
+    auto curentWord = string();
+    auto textPtr = text.data();
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        auto char_ = textPtr[i];
+        if (contains(chars,char_))
+        {
+            words.push_back(curentWord);
+            curentWord.clear();
+        }
+        else
+        {
+            curentWord.push_back(char_);
+        }
+    }
+    words.push_back(curentWord);
+    return words;
+}
+string to_camel_case(string text)
+{
+    auto chars = vector<char> { '-','_' };
+    auto words = split_2(text,chars);
+    for (size_t i = 1; i < words.size(); i++)
+    {
+        string& word = words[i];
+        if (word.size() == 0) continue;
+        word[0] = (char)toupper(word[0]);
+    }
+    return concat(words);
+}
+int main(void)
+{
+    auto test1 = string("the-stealth-warrior");
+    auto test2 = string("The_Stealth_Warrior");
+    auto test3 = string("The_Stealth-Warrior");
+
+    cout << test1 << " -> " << to_camel_case(test1) << endl;
+    cout << test2 << " -> " << to_camel_case(test2) << endl;
+    cout << test3 << " -> " << to_camel_case(test3) << endl;
+
     return 0;
 }
---
+++ projects/ConvertStringToCamelCase2/src/main.cpp
@@ -0,0 +1,26 @@
+#include "base.h"
+
+string to_camel_case(string text)
+{
+    for (int i = 0; i < text.size(); i++)
+    {
+        if (text[i] == '-' || text[i] == '_')
+        {
+            text.erase(i,1);
+            text[i] = (char)toupper(text[i]);
+        }
+    }
+    return text;
+}
+int main(void)
+{
+    auto test1 = string("the-stealth-warrior");
+    auto test2 = string("The_Stealth_Warrior");
+    auto test3 = string("The_Stealth-Warrior");
+
+    cout << test1 << " -> " << to_camel_case(test1) << endl;
+    cout << test2 << " -> " << to_camel_case(test2) << endl;
+    cout << test3 << " -> " << to_camel_case(test3) << endl;
+
+    return 0;
+}
