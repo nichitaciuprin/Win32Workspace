4b526d1fb446f7d887942db20716f9ea037e3449
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-08-14 19:26:12 +0300

    UPDATE

--- projects/HelloWorld/src/main.cpp
+++ projects/HelloWorld/src/main.cpp
@@ -1,12 +1,6 @@
 #include "base.h"
-#include "sockets.h"
 
-#define DEFAULT_PORT "27015"
-
-int __cdecl main(void)
+int main(void)
 {
-    WSADATA wsaData;
-    auto result = WSAStartup(MAKEWORD(2,2), &wsaData);
-    if (result != 0) return 1;
     return 0;
 }
